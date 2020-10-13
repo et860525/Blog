56a8598929e980c031c0780abe5488ed82944fd5
et860525 <et860525@gmail.com>
2020-10-13 15:00:25 +0800

    Some css modified and rename some class

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from blog import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/', views.posts, name='posts'),
-    path('post/', views.post, name='post'),
+    path('post/<slug:slug>', views.post, name='post'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 
+from .models import Post, Tag
+
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html', {})
 
 def posts(request):
-    return render(request, 'blog/posts.html', {})
+    posts = Post.objects.filter(activate=True)
 
-def post(request):
-    return render(request, 'blog/post.html', {})
+    context = {'posts': posts}
+    return render(request, 'blog/posts.html', context)
+
+def post(request, slug):
+    post = Post.objects.get(slug=slug)
+
+    context = {'post': post}
+    return render(request, 'blog/post.html', context)
 
 def about(request):
-    return render(request, 'blog/post.html', {})
+    return render(request, 'blog/about.html', {})
 
 def contact(request):
-    return render(request, 'blog/post.html', {})
+    return render(request, 'blog/contact.html', {})
